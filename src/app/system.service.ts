84e024f30506a9c1bd8be6c4062556c67cf892bf
朝尾直己 <41501869+01397@users.noreply.github.com>
2020-02-29 09:12:37 +0900

    v0.9

--- src/app/system.service.ts
+++ src/app/system.service.ts
@@ -29,7 +29,7 @@ export interface Post {
 })
 export class SystemService {
   public screen: 'start' | 'main' | 'master' = 'start'
-  public version = 'バージョン 0.8'
+  public version = 'バージョン 0.9'
   public statusDoc: AngularFirestoreDocument<Status>
   public tableNames: string[] = []
   public currentClass: number = 0
